f5b96664755e7d76b61462ec07dfd83e7bf643a1
admin <nemiroff4@yahoo.com>
2014-05-02 18:42:39 -0700

    Started work with loading configurations, but it doesn't do the actual loading yet. Also improved saving configurations a bit.

--- Bouncy/src/com/blogspot/mathjoy/bouncy/LoadConfActivity.java
+++ Bouncy/src/com/blogspot/mathjoy/bouncy/LoadConfActivity.java
@@ -4,15 +4,19 @@ import android.media.AudioManager;
 import android.media.SoundPool;
 import android.os.Bundle;
 import android.app.Activity;
+import android.content.SharedPreferences;
 import android.view.Menu;
 import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 public class LoadConfActivity extends Activity
 {
 	public static SoundPool spool = new SoundPool(2, AudioManager.STREAM_MUSIC, 0);
 	public static int button;
 	float buttonVolume = MainActivity.buttonVolume;
-	String[] confNames = new String[100];
+	Spinner chooseConf;
+	String[] confNames;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState)
@@ -21,6 +25,18 @@ public class LoadConfActivity extends Activity
 		setContentView(R.layout.activity_load_conf);
 		setVolumeControlStream(AudioManager.STREAM_MUSIC);
 		button = spool.load(this, R.raw.button, 1);
+		SharedPreferences sp = getSharedPreferences(MyMenu.dataSP, 0);
+		confNames = new String[sp.getInt("numOfConfs", 0)];
+		for (int i = 0; i < sp.getInt("numOfConfs", 0); i++)
+		{
+			confNames[i] = sp.getString(i + "name", "Untitled");
+		}
+		chooseConf = (Spinner) findViewById(R.id.chooseConf);
+		if (confNames[0] != null)
+		{
+			ArrayAdapter<String> confNamesAd = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_dropdown_item, confNames);
+			chooseConf.setAdapter(confNamesAd);
+		}
 	}
 
 	public void goToMainSettings(View v)
--- Bouncy/src/com/blogspot/mathjoy/bouncy/SaveConfActivity.java
+++ Bouncy/src/com/blogspot/mathjoy/bouncy/SaveConfActivity.java
@@ -17,6 +17,12 @@ public class SaveConfActivity extends Activity
 	float buttonVolume = MainActivity.buttonVolume;
 	String[] confNames = new String[100];
 	EditText editName;
+	Toast alreadyExistsT;
+	Toast emptySpaceT;
+	Toast blankT;
+	Toast tooManyT;
+	boolean override = false;
+	String repeatedName = "";
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState)
@@ -26,6 +32,10 @@ public class SaveConfActivity extends Activity
 		setVolumeControlStream(AudioManager.STREAM_MUSIC);
 		button = spool.load(this, R.raw.button, 1);
 		editName = (EditText) findViewById(R.id.editConfName);
+		alreadyExistsT = Toast.makeText(this, "You already have a configuration with that name. Click the save button again to override it.", Toast.LENGTH_LONG);
+		emptySpaceT = Toast.makeText(this, "You must enter a name.", Toast.LENGTH_SHORT);
+		blankT = Toast.makeText(this, "You must have other characters besides a space.", Toast.LENGTH_SHORT);
+		tooManyT = Toast.makeText(this, "You can't have more than 100 configurations.", Toast.LENGTH_SHORT);
 	}
 
 	public void goToMainSettings(View v)
@@ -35,37 +45,89 @@ public class SaveConfActivity extends Activity
 
 	public void saveConf(View v)
 	{
-		boolean save = false;
+		boolean alreadyExists = true;
+		boolean emptySpace = true;
+		boolean tooMany = false;
 		String name = editName.getText().toString();
-		SharedPreferences sp = getSharedPreferences(MyMenu.settingsSP, 0);
+		SharedPreferences sp = getSharedPreferences(MyMenu.dataSP, 0);
+		SharedPreferences sps = getSharedPreferences(MyMenu.settingsSP, 0);
+		int numOfConfs = sp.getInt("numOfConfs", 0);
 		if (sp.getString(name + "name", "1").equals("1"))
 		{
 			if (sp.getString(name + "name", "2").equals("2"))
 			{
-				save = true;
+				alreadyExists = false;
 			}
 		}
-		if (save)
+		for (int i = 0; i < name.length(); i++)
+		{
+			if (name.charAt(i) != ' ' && name.charAt(i) != '	')
+			{
+				emptySpace = false;
+			}
+		}
+		if (numOfConfs >= 100)
+		{
+			tooMany = true;
+		}
+		if (!repeatedName.equals(name))
+		{
+			override = false;
+		}
+		if ((!alreadyExists && !emptySpace && !tooMany) || override)
 		{
 			Editor edit = sp.edit();
-			edit.putString(name + "name", name);
-			edit.putFloat(name + "startBallX", MyView.startBallX);
-			edit.putFloat(name + "startBallXY", MyView.startBallY);
-			edit.putFloat(name + "startBallXSpeed", MyView.startBallXSpeed);
-			edit.putFloat(name + "startBallYSpeed", MyView.startBallYSpeed);
-			edit.putInt(name + "platformsSize", MyView.platforms.size());
+			edit.putString(numOfConfs + "name", name);
+			edit.putFloat(numOfConfs + "startBallX", MyView.startBallX);
+			edit.putFloat(numOfConfs + "startBallXY", MyView.startBallY);
+			edit.putFloat(numOfConfs + "startBallXSpeed", MyView.startBallXSpeed);
+			edit.putFloat(numOfConfs + "startBallYSpeed", MyView.startBallYSpeed);
+			edit.putInt(numOfConfs + "platformsSize", MyView.platforms.size());
+			edit.putInt(numOfConfs + "gravityValue", (int) sps.getFloat("gravityValue", 100));
+			edit.putInt(numOfConfs + "bounceLevelValue", (int) sps.getFloat("bounceLevelValue", 100));
 			for (int i = 0; i < MyView.platforms.size(); i++)
 			{
-				edit.putFloat(name + "platformStartX" + i, MyView.platforms.get(i).getStartX());
-				edit.putFloat(name + "platformStartY" + i, MyView.platforms.get(i).getStartY());
-				edit.putFloat(name + "platformEndX" + i, MyView.platforms.get(i).getEndX());
-				edit.putFloat(name + "platformEndY" + i, MyView.platforms.get(i).getEndY());
+				edit.putFloat(numOfConfs + "platformStartX" + i, MyView.platforms.get(i).getStartX());
+				edit.putFloat(numOfConfs + "platformStartY" + i, MyView.platforms.get(i).getStartY());
+				edit.putFloat(numOfConfs + "platformEndX" + i, MyView.platforms.get(i).getEndX());
+				edit.putFloat(numOfConfs + "platformEndY" + i, MyView.platforms.get(i).getEndY());
 			}
+			edit.putInt("numOfConfs", numOfConfs + 1);
 			edit.commit();
 			onBackPressed();
-		} else
+		} else if (alreadyExists)
 		{
-			Toast.makeText(this, "You already have a configuration with that name", Toast.LENGTH_SHORT).show();
+			override = true;
+			repeatedName = name;
+			alreadyExistsT.cancel();
+			emptySpaceT.cancel();
+			blankT.cancel();
+			tooManyT.cancel();
+			alreadyExistsT.show();
+		} else if (emptySpace)
+		{
+			if (name.length() == 0)
+			{
+				alreadyExistsT.cancel();
+				emptySpaceT.cancel();
+				blankT.cancel();
+				tooManyT.cancel();
+				emptySpaceT.show();
+			} else
+			{
+				alreadyExistsT.cancel();
+				emptySpaceT.cancel();
+				blankT.cancel();
+				tooManyT.cancel();
+				blankT.show();
+			}
+		} else if (tooMany)
+		{
+			alreadyExistsT.cancel();
+			emptySpaceT.cancel();
+			blankT.cancel();
+			tooManyT.cancel();
+			tooManyT.show();
 		}
 	}
 
@@ -73,6 +135,9 @@ public class SaveConfActivity extends Activity
 	public void onBackPressed()
 	{
 		spool.play(button, buttonVolume, buttonVolume, 0, 0, 1);
+		alreadyExistsT.cancel();
+		emptySpaceT.cancel();
+		blankT.cancel();
 		super.onBackPressed();
 	}
 }
