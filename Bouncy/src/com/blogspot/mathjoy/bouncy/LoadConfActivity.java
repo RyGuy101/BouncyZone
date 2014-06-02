caa95d0293e326d7f75b62031d329150cbd45d5d
admin <nemiroff4@yahoo.com>
2014-06-01 20:44:16 -0700

    This should be the version that has been on the google play store

--- Bouncy/src/com/blogspot/mathjoy/bouncy/LoadConfActivity.java
+++ Bouncy/src/com/blogspot/mathjoy/bouncy/LoadConfActivity.java
@@ -5,7 +5,10 @@ import android.media.AudioManager;
 import android.media.SoundPool;
 import android.os.Bundle;
 import android.app.Activity;
+import android.content.Intent;
 import android.content.SharedPreferences;
+import android.content.SharedPreferences.Editor;
+import android.graphics.Color;
 import android.view.Menu;
 import android.view.View;
 import android.widget.ArrayAdapter;
@@ -39,6 +42,11 @@ public class LoadConfActivity extends Activity
 		{
 			ArrayAdapter<String> confNamesAd = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_dropdown_item, confNames);
 			chooseConf.setAdapter(confNamesAd);
+		} else
+		{
+			ArrayAdapter<String> confNamesAd = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_dropdown_item, new String[]
+			{ "You have no saved configurations!" });
+			chooseConf.setAdapter(confNamesAd);
 		}
 	}
 
@@ -49,7 +57,6 @@ public class LoadConfActivity extends Activity
 
 	public void loadConf(View v)
 	{
-		spool.play(button, buttonVolume, buttonVolume, 0, 0, 1);
 		SharedPreferences sp = getSharedPreferences(MyMenu.dataSP, 0);
 		int n = -1;
 		for (int i = 0; i < sp.getInt("numOfConfs", 0); i++)
@@ -59,13 +66,28 @@ public class LoadConfActivity extends Activity
 				n = i;
 			}
 		}
-		ArrayList<Platform> platforms = new ArrayList<Platform>();
 		if (!sp.getString(n + "name", " ").equals(" "))
 		{
-			// for (int i = 0; i < sp.getInt("platformsSize", 0); i++)
-			// {
-			// platforms.add(new Platform(sp.getFloat(n + "platformStartX", 0), sp.getFloat(n + "platformStartY", 0), sp.getFloat(n + "platformEndX", 0), sp.getFloat(n + "platformEndY", 0)));
-			// }
+			spool.play(button, buttonVolume, buttonVolume, 0, 0, 1);
+			MyView.ballX = sp.getFloat(n + "startBallX", 0);
+			MyView.ballY = sp.getFloat(n + "startBallY", 0);
+			MyView.ballXSpeed = sp.getFloat(n + "startBallXSpeed", 0);
+			MyView.ballYSpeed = sp.getFloat(n + "startBallYSpeed", 0);
+			MyView.startBallX = sp.getFloat(n + "startBallX", 0);
+			MyView.startBallY = sp.getFloat(n + "startBallY", 0);
+			MyView.startBallXSpeed = sp.getFloat(n + "startBallXSpeed", 0);
+			MyView.startBallYSpeed = sp.getFloat(n + "startBallYSpeed", 0);
+			SavePrefs("gravityValue", sp.getInt(n + "gravityValue", 100));
+			SavePrefs("bounceLevelValue", sp.getInt(n + "bounceLevelValue", 100));
+			ArrayList<Platform> platforms = new ArrayList<Platform>();
+			for (int i = 0; i < sp.getInt(n + "platformsSize", 0); i++)
+			{
+				platforms.add(new Platform(sp.getFloat(n + "platformStartX" + i, 0), sp.getFloat(n + "platformStartY" + i, 0), sp.getFloat(n + "platformEndX" + i, 0), sp.getFloat(n + "platformEndY" + i, 0)));
+			}
+			MyView.platforms = platforms;
+			Intent intent = new Intent(this, MainActivity.class);
+			intent.putExtra("fromLoad", true);
+			startActivity(intent);
 		}
 	}
 
@@ -74,5 +96,13 @@ public class LoadConfActivity extends Activity
 	{
 		spool.play(button, buttonVolume, buttonVolume, 0, 0, 1);
 		super.onBackPressed();
+	}
+
+	private void SavePrefs(String key, float value)
+	{
+		SharedPreferences sp = getSharedPreferences(MyMenu.settingsSP, 0);
+		Editor edit = sp.edit();
+		edit.putFloat(key, value);
+		edit.commit();
 	}
 }
