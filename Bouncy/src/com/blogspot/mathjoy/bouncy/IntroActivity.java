d2cfd2c60f395f21b37f87e6679061f8b6dface1
Boys <Boys@Boys-HP>
2014-09-20 23:09:39 -0700

    Intro screen now has platforms, but more need to be added.

--- Bouncy/src/com/blogspot/mathjoy/bouncy/IntroActivity.java
+++ Bouncy/src/com/blogspot/mathjoy/bouncy/IntroActivity.java
@@ -5,15 +5,23 @@ import org.jbox2d.dynamics.BodyType;
 
 import android.app.Activity;
 import android.media.AudioManager;
+import android.media.SoundPool;
 import android.os.Bundle;
+import android.view.View;
 
 public class IntroActivity extends Activity
 {
+	public static SoundPool spoolBounce = new SoundPool(5, AudioManager.STREAM_MUSIC, 0);
+	public static int bounce;
+	public static View buttons;
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState)
 	{
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_intro);
 		setVolumeControlStream(AudioManager.STREAM_MUSIC);
+		bounce = spoolBounce.load(this, R.raw.bounce, 1);
+		buttons = findViewById(R.id.introButtons);
 	}
 }
--- Bouncy/src/com/blogspot/mathjoy/bouncy/IntroView.java
+++ Bouncy/src/com/blogspot/mathjoy/bouncy/IntroView.java
@@ -1,11 +1,13 @@
 package com.blogspot.mathjoy.bouncy;
 
+import org.jbox2d.common.Vec2;
 import org.jbox2d.dynamics.BodyType;
 
 import android.content.Context;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.util.AttributeSet;
+import android.view.MotionEvent;
 
 public class IntroView extends MyView
 {
@@ -13,6 +15,7 @@ public class IntroView extends MyView
 	{
 		super(context, attrs);
 		ballColor = Color.GREEN;
+		sp = IntroActivity.spoolBounce;
 	}
 
 	@Override
@@ -24,7 +27,37 @@ public class IntroView extends MyView
 
 	protected void setup()
 	{
+		bounce = IntroActivity.bounce;
 		super.setup();
-		platforms.add(new Platform(BodyType.STATIC, toMeters(100), toMeters(100), toMeters(400), toMeters(400), 0, 1, 0));
+		startBallY = toMeters(100);
+		ball.setPosition(new Vec2(ball.getX(), toMeters(100)));
+		ball.setFriction(0f);
+		ball.setVelocity(new Vec2(5, 0));
+		float w = this.getWidth();
+		float h = this.getHeight();
+		makePlatform(w * 0.02f, w * 0.02f, w - w * 0.02f, w * 0.02f);
+		makePlatform(w * 0.02f, w * 0.02f, w * 0.02f, h - w * 0.02f);
+		makePlatform(w * 0.02f, h - w * 0.02f, w - w * 0.02f, h - w * 0.02f);
+		makePlatform(w - w * 0.02f, w * 0.02f, w - w * 0.02f, h - w * 0.02f);
+
+		makePlatform(IntroActivity.buttons.getLeft(), IntroActivity.buttons.getTop(), IntroActivity.buttons.getRight(), IntroActivity.buttons.getTop());
+		makePlatform(IntroActivity.buttons.getLeft(), IntroActivity.buttons.getBottom(), IntroActivity.buttons.getRight(), IntroActivity.buttons.getBottom());
+		makePlatform(IntroActivity.buttons.getLeft(), IntroActivity.buttons.getTop(), IntroActivity.buttons.getLeft(), IntroActivity.buttons.getBottom());
+		makePlatform(IntroActivity.buttons.getRight(), IntroActivity.buttons.getTop(), IntroActivity.buttons.getRight(), IntroActivity.buttons.getBottom());
+
+//		makePlatform(IntroActivity.buttons.getLeft() + w * 0.1f, w * 0.2f, IntroActivity.buttons.getRight() - w * 0.1f, IntroActivity.buttons.getTop() - w * 0.2f);
+//		makePlatform(IntroActivity.buttons.getLeft() + w * 0.2f, h - w * 0.2f, IntroActivity.buttons.getRight(), IntroActivity.buttons.getBottom() + w * 0.2f);
+//		makePlatform(IntroActivity.buttons.getRight() + w * 0.1f, IntroActivity.buttons.getTop() + IntroActivity.buttons.getHeight() / 2.0f, w - w * 0.2f, IntroActivity.buttons.getBottom());
+	}
+
+	@Override
+	public boolean onTouchEvent(MotionEvent event)
+	{
+		return false;
+	}
+
+	private void makePlatform(float startX, float startY, float endX, float endY)
+	{
+		platforms.add(new Platform(BodyType.STATIC, toMeters(startX), toMeters(startY), toMeters(endX), toMeters(endY), 0, 1, 0));
 	}
 }
